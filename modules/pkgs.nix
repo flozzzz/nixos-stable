36464dbf0e3714f3780262d48a260e3357a3d5eb
flozzzz <stranik_2008@mail.ru>
2026-04-28 07:50:12 +0300

    style(pkgs): organize packages with comments

--- modules/pkgs.nix
+++ modules/pkgs.nix
@@ -1,61 +1,77 @@
 { config, pkgs, lib, ... }:
 
 {
-    #use-unfree
-    nixpkgs.config = {
-    allowUnfree = true;
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Services
+  services.usbmuxd.enable = true;
+
+  # Programs
+  programs.firefox.enable = true;
+  programs.fish.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
   };
- services.usbmuxd.enable = true;
- programs.firefox.enable = true;
- programs.steam = {
-  enable = true;
-  remotePlay.openFirewall = true; 
-  dedicatedServer.openFirewall = true;
-};
- programs.fish.enable = true;
 
+  # System packages
   environment.systemPackages = with pkgs; [
-  usbutils
-  usbmuxd
-  libimobiledevice
-  sptlrx
-  gvfs
-  mtpfs
-  jmtpfs
-  vim
-  sl
-  wget
-  python3
-  neovim
-  btop
-  fastfetch
-  cava
-  pipes-rs
-  kitty
-  git
-  unzip
-  nyancat
-  tty-clock
-  ncdu
-  wayland-protocols
-  quickshell
-  kdePackages.dolphin
-  rsync
-  telegram-desktop
-  libreoffice-qt
-  ffmpeg-full
-  mpv
-  qbittorrent
-  lazarus
-  polkit_gnome
-  obs-studio
-  wineWow64Packages.stable
-  winetricks
-  virtiofsd
-  zip
-  wl-clipboard
-  cliphist
-  rofi
- ];
+    # Core utilities
+    git
+    wget
+    unzip
+    zip
+    rsync
+    ncdu
+    usbutils
 
+    # Device / phone / filesystem helpers
+    usbmuxd
+    libimobiledevice
+    gvfs
+    mtpfs
+    jmtpfs
+    virtiofsd
+
+    # Development / programming
+    python3
+    gcc
+    neovim
+    vim
+    lazarus
+
+    # Terminal / TUI / shell tools
+    btop
+    fastfetch
+    cava
+    sptlrx
+    pipes-rs
+    sl
+    nyancat
+    tty-clock
+
+    # Desktop / Wayland / shell
+    kitty
+    quickshell
+    rofi
+    wl-clipboard
+    cliphist
+    wayland-protocols
+    kdePackages.dolphin
+    polkit_gnome
+
+    # Media / internet / apps
+    telegram-desktop
+    libreoffice-qt
+    ffmpeg-full
+    mpv
+    qbittorrent
+    obs-studio
+
+    # Wine / Windows compatibility
+    wineWow64Packages.stable
+    winetricks
+  ];
 }
