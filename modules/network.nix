ce57715bb988ae0a099cc0301cf59bf41d446d2b
flozzzz <stranik_2008@mail.ru>
2026-06-07 18:08:20 +0300

    update

--- modules/network.nix
+++ modules/network.nix
@@ -4,6 +4,11 @@
  #nftables-and-firewall
  networking.firewall.enable = true;
  networking.nftables.enable = true;
+
+ #homelab-domains
+ networking.hosts = {
+  "192.168.0.15" = [ "npm.home" "jellyfin.home" "nextcloud.home" "adguard.home" "portainer.home" ];
+};
  
  #vless
  programs.throne.enable = true;
