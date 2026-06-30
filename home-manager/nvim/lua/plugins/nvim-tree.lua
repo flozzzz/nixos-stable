27328f62318e9cd4ab595d8e3622cd2b86e0b215
flozzzz <stranik_2008@mail.ru>
2026-06-30 23:13:05 +0300

    fixed bugs in nvim

--- home-manager/nvim/lua/plugins/nvim-tree.lua
+++ home-manager/nvim/lua/plugins/nvim-tree.lua
@@ -1,12 +1,27 @@
 return {
-  'nvim-tree/nvim-tree.lua',
-  enabled = true,
-  lazy = true,
+  "nvim-tree/nvim-tree.lua",
+
   dependencies = {
-    'nvim-tree/nvim-web-devicons',
+    "nvim-tree/nvim-web-devicons",
   },
-  opts = {},
+
+  cmd = {
+    "NvimTreeToggle",
+    "NvimTreeOpen",
+    "NvimTreeClose",
+  },
+
   keys = {
-    { '<leader>n', '<cmd>NvimTreeToggle<CR>', desc = 'Toggle Neotree'}
-  }
+    {
+      "<leader>n",
+      function()
+        require("nvim-tree.api").tree.toggle()
+      end,
+      desc = "Toggle NvimTree",
+    },
+  },
+
+  config = function()
+    require("nvim-tree").setup({})
+  end,
 }
